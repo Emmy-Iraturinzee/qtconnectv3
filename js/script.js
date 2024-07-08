2c26344b9ef9d583d186f5fd79a1d771b7a9fb92
Emmanuel Iraturinze <Emmy-Iraturinzee>
2024-07-08 12:30:36 +0200

    new

--- js/script.js
+++ js/script.js
@@ -153,24 +153,9 @@
     });
 
 
-    // scroll
-    // $('.scrollTo').on('click', function (e) {
-    //   e.preventDefault();
-    //   var target = $(this).attr('href');
-    //   $('html, body').animate({
-    //     scrollTop: ($(target).offset().top)
-    //   }, 500);
-    // });
 
   });
 
 })(jQuery);
 
-  // scroll
-    // $('.scrollTo').on('click', function (e) {
-    //   e.preventDefault();
-    //   var target = $(this).attr('href');
-    //   $('html, body').animate({
-    //     scrollTop: ($(target).offset().top)
-    //   }, 500);
-    // });
+         []
